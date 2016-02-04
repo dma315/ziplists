70c0b8586c1008c4b748a3132422b8fc652520e1
dma315 <dma315@gmail.com>
2016-02-04 14:12:33 -0600

    Add toggle for glyphicon

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 
+
+  // Toggle + / - on accordion
+  $('.panel-group').on('click', '.btn-sm', function(event) {
+    $thisIcon = $(this).find('.glyphicon')
+    if ($thisIcon.attr('class') == "glyphicon glyphicon-plus") {
+      $('.glyphicon').removeClass("glyphicon-minus")
+      $('.glyphicon').addClass("glyphicon-plus")
+      $thisIcon.addClass("glyphicon-minus")  
+    } else {
+      $thisIcon.removeClass("glyphicon-minus")
+      $thisIcon.addClass("glyphicon-plus")
+    }
+  });
+
 });

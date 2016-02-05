9bfa78b854b4e024871cd421fa35bef4047417be
dma315 <dma315@gmail.com>
2016-02-05 12:59:25 -0600

    Complete challenge pre-demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,11 @@ User.destroy_all
 List.destroy_all
 
 u = User.new(first_name: "David", last_name: "Ma", email: "dma315@gmail.com")
+u2 = User.
 u.password = "password"
 u.save
 
-l1 = List.create(title: "My list", creator: u, description: "Awesome list")
-l2 = List.create(title: "My other list", creator: u, description: "Really awesome list")
-l3 = List.create(title: "My third list", creator: u, description: "Really awesome stuff")
+l1 = List.create(title: "Chicago's Best Donuts", creator: u, description: "Fried dough is great!")
+l2 = List.create(title: "Chicago's Best Pizza", creator: u, description: "Pizza is a vegetable")
+l3 = List.create(title: "Chicago's Best Ramen", creator: u, description: "May he touch you with his noodly appendage")
+l4 = List.create(title: "Famous Golf Courses", creator: u, description: "These are probably really expensive")
